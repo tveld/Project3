2dae66f752bc67a30e453689129e0339c1263917
Troy Veldhuizen <veldhuit@mail.gvsu.edu>
2016-12-02 17:07:37 -0500

    make directories. rename files. move files

--- Q2/automation.sh
+++ Q2/automation.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
+
+# unzip file
 unzip -o -qq $*
-ls | gawk -F "_" '$2 != "" {print $2}' | sort -u | while read i; do mkdir $i; done
+
+# make new directory, rename file, move into appropriate directory
+ls | gawk -F "_" '$5 != "" {print $2 " " $5}' | sort -u | while read -r dir fname;
+do
+	mkdir $dir
+	mv *$dir*$fname $fname
+	mv $fname $dir
+done 
