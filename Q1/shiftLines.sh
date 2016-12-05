63213a9b25e0345f2269d6fd38a4d17cbc6f8c13
Gaelen McIntee <mcinteeg@mail.gvsu.edu>
2016-12-05 17:03:10 -0500

    Cleaned root dir

--- Q1/shiftLines.sh
+++ Q1/shiftLines.sh
@@ -12,7 +12,7 @@ fi
 output=""
 while read line
 do
-    output=$output$line$'\n'  # New line isn't working
+    output="$output$line"$'\n'  # New line isn't working
 done < $1
 
 echo $output > outfile
