3a85f67a9ae4a8292b69f525c9df7658a8ed2702
Gaelen McIntee <mcinteeg@mail.gvsu.edu>
2016-12-05 19:35:20 -0500

    rename noise, use gawk in shiftLines

--- Q1/shiftLines.sh
+++ Q1/shiftLines.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
-# Usage: ./shiftLines.sh test.dat
-#              ^$0         ^$1
+# Usage: ./shiftLines.sh test.dat noise.txt
+#              ^$0         ^$1      ^$2
 
 if test $# -eq 0
 then
-    echo "Usage: ./shiftLines.sh <file>"
+    echo "Usage: ./shiftLines.sh <file> <noise file>"
     exit 0
 fi
 
-#build this variable with the shifted lines
-output=""
-while read line
-do
-    output="$output$line"$'\n'  # New line isn't working
-done < $1
 
-echo $output > outfile
+cat $1 | gawk '
+{
+    size = split($0, words, " ")
+
+    for (i = 1; i <= NF; i++)
+        if (system("grep $words[i] $2") != "")
+            delete words[i]
+
+    for (i = 0; i < size; i++)
+        print words[i]
+
+}'
+# output=""
+# while read line
+# do
+#     output=$output$line$'\n' # New line isn't working
+# done < $1
+#
+# echo "$output" > outfile
