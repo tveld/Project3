737d522d0f73e992f1834eeed1e4bc154de13a26
Troy Veldhuizen <veldhuit@mail.gvsu.edu>
2016-12-07 20:23:48 -0500

    fixed remove noise

--- Q1/filterNoiseWords.sh
+++ Q1/filterNoiseWords.sh
@@ -1,5 +1,26 @@
 #! /bin/bash
 
-sedOut="\\b/g;s\\/b"
-sedCommand=$(tr -s '\n' ' ' < $1 | sed 's/ /$sedOut/g')
-echo "$sedCommand"
+> temp
+
+echo
+
+while read stdin
+do
+	echo "$stdin" >> temp
+done
+
+while read noise
+do
+
+	sed -i "s/\b$noise\b//g" temp
+
+done < $1
+
+sed -i 's/  / /g' temp
+
+gawk '{
+	for(i = 1; i < NF; i++){
+		printf("%s ", $i);
+	}
+	printf("%s\n", $NF);
+}' temp | sort -u
